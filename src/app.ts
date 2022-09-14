a0599aacb6d8bd5395ddc3f322b867d71d84a3a8
nicoescudero <nicolas.escudero00@gmail.com>
2022-09-13 21:37:57 -0300

    config app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,44 @@
 import express from 'express';
+import * as http from 'http';
+import * as winston from 'winston';
+import * as expressWinston from 'express-winston';
+import cors from 'cors';
+import debug from 'debug';
+import dotenv from 'dotenv';
+import { CommonRoutesConfig } from './common/common.routes.config';
+import { UserRoutes } from './users/users.routes.config';
 
 const app: express.Application = express();
+const server: http.Server = http.createServer(app);
+const debugLog: debug.IDebugger = debug('app');
+const dotenvResult = dotenv.config();
+const routes: Array<CommonRoutesConfig> = [];
+const port = 3000;
 
-app.listen(3000,()=>console.log(`Server on port 3000`));
+const loggerOptions: expressWinston.LoggerOptions = {
+  transports: [new winston.transports.Console],
+  format: winston.format.combine(
+    winston.format.json(),
+    winston.format.prettyPrint(),
+    winston.format.colorize({ all: true}),
+    ),
+};
 
-export  default app;
+app.use(express.json());
+app.use(cors());
+
+if(dotenvResult.error) throw dotenvResult.error;
+if(!process.env.DEBUG) loggerOptions.meta = false;
+
+
+app.use(expressWinston.logger(loggerOptions));
+routes.push(new UserRoutes(app));
+app.get('/',(req: express.Request, res: express.Response) => { return res.send('Server on port 3000')});
+
+server.listen(port, ()=>{
+  routes.forEach((route: CommonRoutesConfig) => {
+    debugLog(`Routes configured for ${route.name}`);
+  });
+});
+
+export default app;
