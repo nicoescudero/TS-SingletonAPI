15ba11d17934a2ca92c923416a17e995a54354ea
nicoescudero <nicolas.escudero00@gmail.com>
2022-09-16 22:22:22 -0300

    add: mongoose-service

---
+++ src/common/services/mongoose.service.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+import debug from 'debug';
+
+const log: debug.IDebugger = debug('app:mongoose-service');
+
+class MongooseConnection{
+  private count = 0;
+  private options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    useFindAndModify: false,
+  };
+
+  constructor(){
+    this.connectWithRetry();
+  }
+  
+  getMongoose(){
+    return mongoose;
+  }
+  
+  async connectWithRetry(){
+    try {
+      log('Attempting MongoDB connection (will retry if needed)');
+      await mongoose.connect('mongodb://localhost/chat', this.options);
+      console.log('MongoDB connected');
+    } catch (error) {
+      console.log(`Failed connection to mongoDB ${error}`);
+      const retrySeconds = 5;
+      log(`MongoDB connection unsuccessful (will retry #${++this.count} after ${retrySeconds} seconds):`, error);
+      setTimeout(this.connectWithRetry, retrySeconds * 1000);
+    }
+  }
+}
+
+export default new MongooseConnection();
